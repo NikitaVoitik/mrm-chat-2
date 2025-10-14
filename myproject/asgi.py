a6b6c8ebe9ba2067dcf1b3b8fe5bfebba724e605
nikitavoitik <nikitavoitik2006@gmail.com>
2025-10-14 16:32:05 +0200

    Ensure Django setup in ASGI configuration for proper initialization

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -8,12 +8,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+# Set Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+
+# Initialize Django
+django.setup()
+
+# Now import Django-dependent modules
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 django_asgi_app = get_asgi_application()
 
