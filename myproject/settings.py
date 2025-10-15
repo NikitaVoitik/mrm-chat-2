45c5729cc34553279c874a33ba32166fb0bf83d7
nikitavoitik <nikitavoitik2006@gmail.com>
2025-10-15 09:55:45 +0200

    Configure PostgreSQL database integration and update dependencies
    - Replace SQLite with PostgreSQL as the default database.
    - Add `python-dotenv` for environment variable management.
    - Add `psycopg2-binary` to handle PostgreSQL connections.
    - Migrate models with additional help texts for improved documentation.

--- myproject/settings.py
+++ myproject/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,8 +97,12 @@ ASGI_APPLICATION = 'myproject.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'mrm'),
+        'USER': os.environ.get('DB_USER', 'mrm_user'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'dpg-d3nkr7m3jpic73c0hi80-a.oregon-postgres.render.com'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
